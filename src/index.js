3c67b70120df690e10da056609870b7a6d807dc0
Karmanchikova <karmanchikovaoleksandra@gmail.com>
2022-07-23 12:00:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,9 +23,9 @@ function formatDay(timestamp) {
   return days[day];
 }
 function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
@@ -58,9 +58,10 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "a28cec4bab57c9cd0fb85c6d20697f64";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
